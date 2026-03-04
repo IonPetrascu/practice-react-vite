a96169f753f0ee00865f86723790959e0414699e
Ion Petrascu <ionpetrascuc0318@gmail.com>
2026-03-04 18:52:11 +0200

    feat: show version of app in console

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { version } from './package.json';
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    __APP_VERSION__: JSON.stringify(version),
+  },
   plugins: [react(), tailwindcss()],
   test: {
     environment: 'jsdom',
