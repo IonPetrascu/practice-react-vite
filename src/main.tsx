9a2bc28d3d2677bde49a4d92503213a014652a83
Ion Petrascu <ionpetrascuc0318@gmail.com>
2026-03-06 14:10:33 +0200

    fix: disable eslint warning for log of App version

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import Providers from './app/providers';
 
+// eslint-disable-next-line no-console
 console.log(`App version: ${__APP_VERSION__}`);
 
 createRoot(document.getElementById('root')!).render(
